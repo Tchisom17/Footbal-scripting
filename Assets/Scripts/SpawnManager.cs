8590a98c5790207ccc97e0402b5634f6a2896ccb
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-09-21 03:29:24 +0100

    Feat:create a method to return spawn position

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,19 +6,23 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+
     // Start is called before the first frame update
     void Start()
     {
-        float spawnPosx = Random.Range(-spawnRange, spawnRange);
-        float spawnPosz = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosx, 0, spawnPosz);
-        
-        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    private Vector3 GenerateSpawnPosition()
     {
-        
+        float spawnPosx = Random.Range(-spawnRange, spawnRange);
+        float spawnPosz = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosx, 0, spawnPosz);
+
+        return randomPos;
     }
 }
